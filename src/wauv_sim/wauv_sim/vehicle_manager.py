11015c75181163caeedb182c5585953e5b6d50fd
Keiji Toriumi <keijitoriumi89@gmail.com>
2026-04-20 21:46:37 -0500

    Rewrote vehicle_manager to fix spin issue

--- src/wauv_sim/wauv_sim/vehicle_manager.py
+++ src/wauv_sim/wauv_sim/vehicle_manager.py
@@ -1,7 +1,6 @@
 """
 Filename: vehicle_manager.py
 Author: Keiji Toriumi
-Date: 10/03/2026
 Description: ROS2 node to wait for vehicle status and manage AUV operation
 """
 import rclpy
@@ -11,6 +10,7 @@ from std_msgs.msg import Bool
 
 from mavros_msgs.srv import CommandBool
 from mavros_msgs.srv import SetMode
+from mavros_msgs.msg import State
 
 class VehicleManager(Node):
 
@@ -35,6 +35,13 @@ class VehicleManager(Node):
             '/mavros/set_mode'
         )
 
+        self.state_sub = self.create_subscription(
+            State,
+            '/mavros/state',
+            self.state_callback,
+            10
+        )
+
         self.get_logger().info("Waiting for MAVROS services...")
         self.arm_client.wait_for_service()
         self.mode_client.wait_for_service()
@@ -45,6 +52,13 @@ class VehicleManager(Node):
 
         # track if the mode has been set to GUIDED
         self.mode_set = False
+
+        # track AUV state
+        self.state = State()
+
+        # prevent repeated service spam
+        self.mode_request_sent = False
+        self.arm_request_sent = False
 
         # main loop
         self.timer = self.create_timer(
@@ -57,40 +71,56 @@ class VehicleManager(Node):
         req.custom_mode = mode
 
         future = self.mode_client.call_async(req)
+        future.add_done_callback(self.mode_response)
 
-        # spin until mode change request
-        rclpy.spin_until_future_complete(self, future)
+    def mode_response(self, future):
+        res = future.result()
 
-        if future.result().mode_sent:
-            self.get_logger().info(f"Mode set to {mode}")
+        if res.mode_sent:
+            self.get_logger().info("Mode set to GUIDED")
+            self.mode_set = True
         else:
             self.get_logger().warn("Mode change failed")
+            self.mode_request_sent = False  # allow retry
 
     def arm_vehicle(self):
         req = CommandBool.Request()
         req.value = True
 
         future = self.arm_client.call_async(req)
+        future.add_done_callback(self.arm_response)
 
-        rclpy.spin_until_future_complete(self, future)
+    def arm_response(self, future):
+        res = future.result()
 
-        if future.result().success:
+        if res.success:
             self.get_logger().info("Vehicle armed")
             self.vehicle_ready = True
         else:
             self.get_logger().warn("Arming failed")
+            self.arm_request_sent = False  # allow retry
+
+    def state_callback(self, msg):
+        self.state = msg
 
     def manager_loop(self):
+        # wait for FCU connection
+        if not self.state.connected:
+            self.get_logger().info("Waiting for FCU...")
+            return
+
         # make sure mode is guided
         if not self.mode_set:
-            self.set_mode("GUIDED")
-            self.mode_set = True
+            if not self.mode_request_sent:
+                self.set_mode("GUIDED")
+                self.mode_request_sent = True
             return
 
         # make sure vehicle is ready
         if not self.vehicle_ready:
-            # Arm vehicle
-            self.arm_vehicle()
+            if not self.arm_request_sent:
+                self.arm_vehicle()
+                self.arm_request_sent = True
             return
 
         msg = Bool()
